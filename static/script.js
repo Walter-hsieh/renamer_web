8eb04450f72d7efa6fb5a2960cc088b64c29d0f2
Tsung-Lin Hsieh <120996655+Walter-hsieh@users.noreply.github.com>
2024-10-02 15:45:11 +0800

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    // Custom file input label display
+    $('.custom-file-input').on('change', function () {
+        var fileName = $(this).val().split('\\').pop();
+        $(this).siblings('.custom-file-label').addClass('selected').html(fileName);
+    });
+
+    // Ajax form submission
+    $('#uploadForm').on('submit', function (e) {
+        e.preventDefault();
+        var formData = new FormData(this);
+
+        $.ajax({
+            url: '/upload',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (response) {
+                if (response.error) {
+                    alert(response.error);
+                } else {
+                    var downloadUrl = '/processed/' + response.filename;
+                    console.log("Download URL: " + downloadUrl); // Debug line to check URL
+                    $('#result').removeClass('d-none');
+                    $('#downloadLink').attr('href', downloadUrl);
+                    $('#downloadLink').text('Download ' + response.filename);
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {  // Updated error callback for more detailed debugging
+                console.error('Error: ' + textStatus, errorThrown);
+                alert('An error occurred while processing the file. Please try again.');
+            }
+        });
+    });
+});
